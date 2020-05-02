9bcbf3fb2b1d50c3723d32fa20680158ea51e708
Renan <rollorenan@gmail.com>
2020-05-02 16:49:05 -0300

    Adding more helpers to simplify test code

--- DataTests/UseCases/RemoteAddAccountTests.swift
+++ DataTests/UseCases/RemoteAddAccountTests.swift
@@ -4,7 +4,7 @@ import Data
 
 class RemoteAddAccountTests: XCTestCase {
     func test_add_should_call_httpClient_with_correct_url() {
-        let url = URL(string: "http://any-url.com")!
+        let url = makeUrl()
         let (sut, httpClientSpy) = makeSut(url: url)
         sut.add(addAccountModel: makeAddAccountModel()) { _ in }
         XCTAssertEqual(httpClientSpy.urls, [url])
@@ -35,7 +35,7 @@ class RemoteAddAccountTests: XCTestCase {
     func test_add_should_complete_with_error_if_client_completes_with_invalid_data() {
         let (sut, httpClientSpy) = makeSut()
         expect(sut, completeWith: .failure(.unexpected), when: {
-            httpClientSpy.completeWithData(Data("Invalid_data".utf8))
+            httpClientSpy.completeWithData(makeInvalidData())
         })
     }
 }
@@ -67,6 +67,14 @@ extension RemoteAddAccountTests {
     
     func makeAccountModel() -> AccountModel {
         return AccountModel(id: "any_id", name: "any_name", email: "any_email@email.com", password: "any_password")
+    }
+    
+    func makeInvalidData() -> Data {
+        return Data("Invalid_data".utf8)
+    }
+    
+    func makeUrl() -> URL {
+        return URL(string: "http://any-url.com")!
     }
     
     class HttpClientSpy: HttpPostClient {
