ab6708a7c25f45a8c3d6cbf4589f6ffe3c659ad8
Renan <rollorenan@gmail.com>
2020-05-03 23:06:03 -0300

    Ensure SignUpPresenter shows loading before addAccount

--- Presentation/Presenters/SignUpPresenter.swift
+++ Presentation/Presenters/SignUpPresenter.swift
@@ -5,17 +5,20 @@ public final class SignUpPresenter {
     private let alertView: AlertView
     private let emailValidator: EmailValidator
     private let addAccount: AddAccount
+    private let loadingView: LoadingView
     
-    public init(alertView: AlertView, emailValidator: EmailValidator, addAccount: AddAccount) {
+    public init(alertView: AlertView, emailValidator: EmailValidator, addAccount: AddAccount, loadingView: LoadingView) {
         self.alertView = alertView
         self.emailValidator = emailValidator
         self.addAccount = addAccount
+        self.loadingView = loadingView
     }
     public func signUp(viewModel: SignUpViewModel) {
        if let message = validate(viewModel: viewModel) {
             alertView.showMessage(viewModel: AlertViewModel(title: "Validation fails", message: message))
        } else {
         let addAccountModel = AddAccountModel(name: viewModel.name!, email: viewModel.email!, password: viewModel.password!, passwordConfirmation: viewModel.passwordConfirmation!)
+        loadingView.display(viewModel: LoadingViewModel(isLoading: true))
         addAccount.add(addAccountModel: addAccountModel) { [weak self] result in
             guard let self = self else { return }
             switch result {
